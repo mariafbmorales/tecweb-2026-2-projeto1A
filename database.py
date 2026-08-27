c030e9527cb0254a49abea0aec23ef5f762a2442
mariafbmorales <nanda031006@gmail.com>
2026-08-27 10:38:29 -0300

    mudancas

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ class Note:
     id: int = None
     title: str = None
     content: str = ''
-
+    favorite: int = 0
 
 class Database():
     def __init__(self, nome):
@@ -40,7 +40,8 @@ class Database():
     def get_all(self):
         cursor = self.conn.execute(
             '''
-            SELECT id, title, content FROM note
+            SELECT id, title, content, favorite FROM note
+            ORDER BY favorite DESC, id ASC
             '''
         )
 
@@ -50,7 +51,8 @@ class Database():
             note = Note(
                 id=linha[0],
                 title=linha[1],
-                content=linha[2]
+                content=linha[2],
+                favorite=linha[3]
             )
             notes.append(note)
 
@@ -59,12 +61,11 @@ class Database():
     def get_by_id(self, note_id):
         cursor = self.conn.execute(
             '''
-            SELECT id, title, content FROM note
+            SELECT id, title, content, favorite FROM note
             WHERE id = ?;
             ''',
             (note_id,)
         )
-        self.conn.commit()
 
         linha = cursor.fetchone()
 
@@ -74,7 +75,8 @@ class Database():
         return Note(
             id=linha[0],
             title=linha[1],
-            content=linha[2]
+            content=linha[2],
+            favorite=linha[3]
         )
 
 
